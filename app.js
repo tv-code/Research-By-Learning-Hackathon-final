62c786ca2ad377e3afd0b89332715594d9f8ec2a
Tvisha Pushkar <78961285+tv-code@users.noreply.github.com>
2022-04-10 21:08:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+mybutton = document.getElementById("myBtn");
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+  document.documentElement.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
